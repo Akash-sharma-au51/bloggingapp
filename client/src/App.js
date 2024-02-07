613b912fedfe7aac8753cdaf5fc66e171fe49828
Akash Sharma <sharmaakash11401@gmail.com>
2024-02-07 22:15:40 +0530

    routes created

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
+import Home from './component/Home';
+import Login from './component/Login'; 
+import Register from './component/Register'; 
+import New from './component/New'; 
+import Navbar from './component/Navbar'; 
+import Footer from './component/Footer'; 
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />} >
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="/login" element={<Layout />} >
+          <Route index element={<Login/>} />
+        </Route>
+        <Route path="/register" element={<Layout />} >
+          <Route index element={<Register />} />
+        </Route>
+        <Route path="/new" element={<Layout/>} >
+          <Route index element={<New />} />
+        </Route>
+        
+      </Routes>
+    </Router>
   );
-}
+};
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
 export default App;
